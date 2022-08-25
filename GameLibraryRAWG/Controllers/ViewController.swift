b9e238e248bcedbe188e2413c4d3828f825035da
PavelPeacow <68381498+PavelPeacow@users.noreply.github.com>
2022-08-25 14:42:49 +0500

    basic template

---
+++ GameLibraryRAWG/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameLibraryRAWG
+//
+//  Created by Павел Кай on 25.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+
+}
+
