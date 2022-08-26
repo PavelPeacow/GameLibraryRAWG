ef90a5f40ef21b7c9fe40f656a065ca8ceb329cd
PavelPeacow <68381498+PavelPeacow@users.noreply.github.com>
2022-08-26 19:42:16 +0500

    api search implementation
    basic implementation of searching items in tableView

--- GameLibraryRAWG/APIManager/APICaller.swift
+++ GameLibraryRAWG/APIManager/APICaller.swift
@@ -15,7 +15,7 @@ final class APICaller {
     static let shared = APICaller()
     
     func fetchGames(onCompletion: @escaping (Result<[Game], Error>) -> Void) {
-        guard let url = URL(string: "\(APIConstants.BASE_URL)/games?key=\(APIConstants.API_KEY)&page_size=50") else { return }
+        guard let url = URL(string: "\(APIConstants.BASE_URL)/games?key=\(APIConstants.API_KEY)&page_size=20") else { return }
         
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
             
@@ -29,10 +29,33 @@ final class APICaller {
                 return
             }
 
-            print(results)
             onCompletion(.success(results.results))
             
         }
         task.resume()
     }
+    
+    
+    func searchGames(with query: String, onCompletion: @escaping (Result<[Game], Error>) -> Void) {
+        
+        guard let query = query.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else { return }
+        
+        guard let url = URL(string: "\(APIConstants.BASE_URL)/games?key=\(APIConstants.API_KEY)&page_size=20&search=\(query)") else { return }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+
+            guard let results = try? JSONDecoder().decode(GamesResponse.self, from: data) else {
+                onCompletion(.failure(APIError.failedToGetData))
+                return
+            }
+            
+            onCompletion(.success(results.results))
+        }
+        task.resume()
+        
+    }
+
 }
---
+++ GameLibraryRAWG/Controllers/Main/SearchViewController.swift
@@ -0,0 +1,103 @@
+//
+//  SearchViewController.swift
+//  GameLibraryRAWG
+//
+//  Created by Павел Кай on 25.08.2022.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    private var games = [Game]()
+    
+    private var searchTable: UITableView = {
+        let searchTable = UITableView(frame: .zero, style: .grouped)
+        searchTable.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return searchTable
+    }()
+    
+    private var seacrhController: UISearchController = {
+        let searchController = UISearchController(searchResultsController: nil)
+        return searchController
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(searchTable)
+        
+        setDelegates()
+        fetchGames()
+        configureNavBar()
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        searchTable.frame = view.bounds
+    }
+    
+    private func setDelegates() {
+        searchTable.delegate = self
+        searchTable.dataSource = self
+        
+        seacrhController.searchResultsUpdater = self
+    }
+    
+    private func fetchGames() {
+        APICaller.shared.fetchGames { [weak self] result in
+            switch result {
+            case .success(let games):
+                DispatchQueue.main.async {
+                    self?.games = games
+                    self?.searchTable.reloadData()
+                }
+               
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    private func configureNavBar() {
+        navigationItem.searchController = seacrhController
+    }
+    
+}
+
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        games.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = games[indexPath.row].name
+        
+        return cell
+    }
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchBar = searchController.searchBar
+        
+        guard let query = searchBar.text else { return }
+        
+        APICaller.shared.searchGames(with: query) { [weak self] result in
+            switch result {
+            case .success(let games):
+                DispatchQueue.main.async {
+                    self?.games = games
+                    self?.searchTable.reloadData()
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
+    }
+}
