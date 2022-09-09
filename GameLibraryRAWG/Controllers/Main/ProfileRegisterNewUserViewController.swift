3514e9b14c5bac02b3cfdbc034d88537b69440ef
PavelPeacow <68381498+PavelPeacow@users.noreply.github.com>
2022-09-09 19:45:14 +0500

    user registration view add

--- GameLibraryRAWG/Controllers/Main/ProfileRegisterNewUserViewController.swift
+++ GameLibraryRAWG/Controllers/Main/ProfileRegisterNewUserViewController.swift
@@ -1,14 +1,14 @@
 //
-//  ProfileAuthorizationViewController.swift
+//  ProfileRegisterNewUserViewController.swift
 //  GameLibraryRAWG
 //
-//  Created by Павел Кай on 08.09.2022.
+//  Created by Павел Кай on 09.09.2022.
 //
 
 import UIKit
 import FirebaseAuth
 
-class ProfileAuthorizationViewController: UIViewController {
+class ProfileRegisterNewUserViewController: UIViewController {
     
     private let emailTextField: UITextField = {
         let emailTextField = UITextField()
@@ -34,40 +34,40 @@ class ProfileAuthorizationViewController: UIViewController {
         return passwordTextField
     }()
     
-    private let signInButton: UIButton = {
+    private let repeatPasswordTextField: UITextField = {
+        let passwordTextField = UITextField()
+        passwordTextField.placeholder = "Repeat Password"
+        passwordTextField.clearButtonMode = .whileEditing
+        passwordTextField.borderStyle = .roundedRect
+        passwordTextField.autocorrectionType = .no
+        passwordTextField.isSecureTextEntry = true
+        passwordTextField.enablesReturnKeyAutomatically = true
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        return passwordTextField
+    }()
+    
+    private let registerButton: UIButton = {
         let signInButton = UIButton(configuration: UIButton.Configuration.filled())
-        signInButton.setTitle("Sign In", for: .normal)
+        signInButton.setTitle("Register", for: .normal)
         
         signInButton.translatesAutoresizingMaskIntoConstraints = false
         return signInButton
     }()
-    
-    private let dontHaveAccountButton: UIButton = {
-        let dontHaveAccountButton = UIButton(configuration: UIButton.Configuration.bordered())
-        dontHaveAccountButton.setTitle("Don't have an account?", for: .normal)
-        dontHaveAccountButton.translatesAutoresizingMaskIntoConstraints = false
-        return dontHaveAccountButton
-    }()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         view.backgroundColor = .systemBackground
         
         view.addSubview(emailTextField)
         view.addSubview(passwordTextField)
-        view.addSubview(signInButton)
-        view.addSubview(dontHaveAccountButton)
+        view.addSubview(repeatPasswordTextField)
+        view.addSubview(registerButton)
         
-        title = "Profile"
-        navigationController?.navigationBar.prefersLargeTitles = true
+        addActiontoRegisterButton()
         
-        addActionToSignInButton()
-        addActionToDontHaveAccountButton()
-        
-        setConstraints()
         setDelegates()
-        
+        setConstraints()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -80,47 +80,43 @@ class ProfileAuthorizationViewController: UIViewController {
         passwordTextField.delegate = self
     }
     
-    private func addActionToSignInButton() {
-        signInButton.addAction(UIAction(handler: { [weak self] _ in
-            self?.didTapSignInButton()
+    private func addActiontoRegisterButton() {
+        registerButton.addAction(UIAction(handler: { [weak self] _ in
+            self?.didTapRegisterButton()
         }), for: .touchUpInside)
     }
     
-    private func addActionToDontHaveAccountButton() {
-        dontHaveAccountButton.addAction(UIAction(handler: { [weak self] _ in
-            let vc = ProfileRegisterNewUserViewController()
-            self?.present(vc, animated: true)
-        }), for: .touchUpInside)
-    }
-    
-    private func didTapSignInButton() {
-        guard let email = emailTextField.text, !email.isEmpty,
-              let password = passwordTextField.text, !password.isEmpty else { return }
+    private func didTapRegisterButton() {
+        guard let email = emailTextField.text, !email.isEmpty, email.contains("@"),
+              let password = passwordTextField.text, !password.isEmpty, password.count > 6,
+              let repeatPassword = repeatPasswordTextField.text, password == repeatPassword  else {
+            print("You need to be satisfied all")
+            return
+        }
         
-        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password) { [weak self] result, error in
+        FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password) { [weak self] result, error in
             guard error == nil else {
-                print("No such user found!")
+                print("Error when creating user")
                 return
             }
-            
-            print("signed!!!")
-            self?.showSignInAlert()
-            
-           
+            self?.showCreateAccount()
+            print("User created")
         }
+        
+        
+        
+        
     }
-    
-    private func showSignInAlert() {
-        let ac = UIAlertController(title: "Nice", message: "You are sign in!", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "hooray!", style: .default, handler: { [weak self] _ in
-            let vc = UIViewController()
-            self?.navigationController?.setViewControllers([vc], animated: true)
+    	
+    private func showCreateAccount() {
+        let ac = UIAlertController(title: "Your account have been created", message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .cancel, handler: { [weak self] _ in
+            self?.dismiss(animated: true)
         }))
-                     
         present(ac, animated: true)
     }
-    
-    private func setConstraints() {
+
+    func setConstraints() {
         NSLayoutConstraint.activate([
             
             emailTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 70),
@@ -133,21 +129,22 @@ class ProfileAuthorizationViewController: UIViewController {
             passwordTextField.heightAnchor.constraint(equalToConstant: 40),
             passwordTextField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7),
             
-            signInButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 30),
-            signInButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            signInButton.heightAnchor.constraint(equalToConstant: 50),
-            signInButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7),
+            repeatPasswordTextField.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 30),
+            repeatPasswordTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            repeatPasswordTextField.heightAnchor.constraint(equalToConstant: 40),
+            repeatPasswordTextField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7),
             
-            dontHaveAccountButton.topAnchor.constraint(equalTo: signInButton.bottomAnchor, constant: 15),
-            dontHaveAccountButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            dontHaveAccountButton.heightAnchor.constraint(equalToConstant: 50),
-            dontHaveAccountButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7),
+            registerButton.topAnchor.constraint(equalTo: repeatPasswordTextField.bottomAnchor, constant: 30),
+            registerButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            registerButton.heightAnchor.constraint(equalToConstant: 50),
+            registerButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7),
+            
         ])
     }
-    
+
 }
 
-extension ProfileAuthorizationViewController: UITextFieldDelegate {
+extension ProfileRegisterNewUserViewController: UITextFieldDelegate {
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if textField == emailTextField {
@@ -161,5 +158,4 @@ extension ProfileAuthorizationViewController: UITextFieldDelegate {
     override func resignFirstResponder() -> Bool {
         true
     }
-    
 }
